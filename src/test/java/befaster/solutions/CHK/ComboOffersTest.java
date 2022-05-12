00cabe478b28c02acb5bdce2d1017c344b1ed739
sbx_user1051 <sbx_user1051@169.254.118.121>
2022-05-12 15:05:36 +0000

    Thu May 12 15:05:36 UTC 2022

--- src/test/java/befaster/solutions/CHK/ComboOffersTest.java
+++ src/test/java/befaster/solutions/CHK/ComboOffersTest.java
@@ -34,9 +34,9 @@ public class ComboOffersTest extends TestCase {
         ComboOffers comboOffers = new ComboOffers();
 
         List<String> unchosenSkus = new ArrayList<>();
-        // simulate an offer where can buy any two of items A, B or C for a fixed amount
+        // simulate an offer where can buy any three of items S, T, X, Y, Z for a fixed amount
         char[] involvedSkus = {'S', 'T', 'X', 'Y', 'Z'};
-        int reqNoItems = 2;
+        int reqNoItems = 3;
         for (char thisSku : involvedSkus) {
             for (int itemID = 0; itemID < reqNoItems; itemID++) {
                 // need as many of each sku as there are req number of items, to allow for repetition, eg someone buys "AAA"
@@ -46,8 +46,8 @@ public class ComboOffersTest extends TestCase {
 
         comboOffers.makeCombinations(unchosenSkus, reqNoItems, "");
         Set<String> allPossibleCombos = comboOffers.getCombos();
-        // there should be 6 combinations of 3 letters in 2 positions without repeats (eg AB and BA)
-        assert (Objects.equals(allPossibleCombos.size(), 6));
+        // 'I think!' there should be 35 combinations of 5 letters in 3 positions without repeats (ie treat AB and BA as the same)
+        assert (Objects.equals(allPossibleCombos.size(), 35));
 
     }
 }
