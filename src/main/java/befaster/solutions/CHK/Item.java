e7211363e1feb5b38650f61a7fab1dbeffda6e2a
sbx_user1051 <sbx_user1051@169.254.118.121>
2022-05-12 08:21:21 +0000

    Thu May 12 08:21:21 UTC 2022

--- src/main/java/befaster/solutions/CHK/Item.java
+++ src/main/java/befaster/solutions/CHK/Item.java
@@ -1,2 +1,28 @@
-package befaster.solutions.CHK;public class Item {
+package befaster.solutions.CHK;
+
+public class Item {
+    private char SKU;
+    private int fullPrice;
+
+    public Item(char SKU, int fullPrice) {
+        this.SKU = SKU;
+        this.fullPrice = fullPrice;
+    }
+
+    public char getSKU() {
+        return SKU;
+    }
+
+    public void setSKU(char SKU) {
+        this.SKU = SKU;
+    }
+
+    public int getFullPrice() {
+        return fullPrice;
+    }
+
+    public void setFullPrice(int fullPrice) {
+        this.fullPrice = fullPrice;
+    }
 }
+
--- src/main/java/befaster/solutions/CHK/SpecialOffer.java
+++ src/main/java/befaster/solutions/CHK/SpecialOffer.java
@@ -1,17 +1,29 @@
 package befaster.solutions.CHK;
 
-public class SpecialOffer {
+import java.util.HashMap;
+import java.util.Map;
+
+public class SpecialOffer implements Comparable<SpecialOffer> {
     private int offerPrice;
     private String itemsPurchased;
     private int saving;
-    private Map<Character,Item> mapOfValidItems;
+    private Map<Character,Item> mapOfValidItems = new HashMap<>();
 
     public SpecialOffer(int offerPrice, String itemsPurchased, Map<Character, Item> mapOfValidItems) {
         this.offerPrice = offerPrice;
         this.itemsPurchased = itemsPurchased;
         this.mapOfValidItems = mapOfValidItems;
-        for(char itemSku:itemsPurchased){
-            
+        int fullPriceTotal = 0;
+        for(char thisSKU:itemsPurchased.toCharArray()){
+            // get full price of thisSKU and add to fullPriceTotal
+            // saving = fullPriceTotal - offerPrice
+            // need to write Item separately first
         }
     }
+
+    @Override
+    public int compareTo(SpecialOffer o) {
+        return 0;
+    }
 }
+
